6af411186b8a480648cf3b98c430f2b57d628d03
amitoje2000 <68534172+amitoje2000@users.noreply.github.com>
2020-07-20 13:16:02 +0530

    Create Assignment Day1.py

---
+++ Assignment Day1.py
@@ -0,0 +1,11 @@
+1) What is Python? What are the ways to install Python on your PC?
+
+ans-Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.
+Steps to Install Python in Your Pc
+
+Step 1: Download the Python 3 Installer. Open a browser window and navigate to the Download page for Windows at python.org. ...
+Step 2: Run the Installer. Once you have chosen and downloaded an installer, simply run it by double-clicking on the downloaded file.
+
+2) What are Code Environments? Types ?
+
+Ans-A code environment is a standalone and self-contained environment to run Python or R code. Each code environment has its own set of packages. Environments are independent: you can install different packages or different versions of packages in different environments without interaction between them.Pycharm, VScCode, Sypder, Jyupter Notebook etc..are the various environments for Python
